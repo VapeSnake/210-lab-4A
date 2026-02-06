3030b271b2195a0df5c7e0eb9545ebdba2f6a5e6
Noel Mier-Luna <nmierluna584@insite.4cd.edu>
2026-02-05 19:15:22 -0800

    Initial commit

---
+++ 210-lab-4A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
+
+int main() {
+    cout << "Hello world!" << endl;
+    return 0;
+}
