aaa944487d8ea0ca60c620e7ccfe890c7d4c914a
Noel Mier-Luna <nmierluna584@insite.4cd.edu>
2026-02-05 20:16:59 -0800

    8th Commit: Messing with setw for table

--- 210-lab-4A.cpp
+++ 210-lab-4A.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 const int MIN_VALUE = 25;
 const int MAX_VALUE = 50;
+const int TABLE_WIDTH = 15;
 
 struct Color
 {
@@ -29,16 +30,17 @@ int main()
     for (int i = 0; i < n; i++)
     {
         Color tempColor = {rand() % 256, rand() % 256, rand() % 256}; // Random RGB values between 0 and 255
-        colorVector.push_back(tempColor);                             // Push struct object into vector
+        colorVector[i] = tempColor;   // Initializes current index of vector to temp Color object
         cout << " Color " << i + 1 << ": (" << colorVector[i].red << ", " << colorVector[i].green << ", "
              << colorVector[i].blue << ")" << endl; // Output RGB values of current color
     }
-    cout << left << setw(10) << "Color #" << setw(10) << "Red" << setw(10) << "Green" << setw(10) << "Blue" << endl;
+    cout << left << setw(TABLE_WIDTH) << "Color #" << setw(TABLE_WIDTH) << "Red" << setw(TABLE_WIDTH) << "Green" << setw(TABLE_WIDTH) << "Blue" << endl;
+    
     // Table headers
     for (int i = 0; i < colorVector.size(); i++)
     {
-        cout << left << setw(10) << i + 1 << setw(10) << colorVector[i].red << setw(10)
-             << colorVector[i].green << setw(10) << colorVector[i].blue << endl;
+        cout << left << setw(TABLE_WIDTH) << i + 1 << setw(TABLE_WIDTH) << colorVector[i].red << setw(TABLE_WIDTH)
+             << colorVector[i].green << setw(TABLE_WIDTH) << colorVector[i].blue << endl;
     }
     return 0;
 }
