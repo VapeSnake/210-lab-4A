50a896a290591f46afacd11140acb5e8929a8a44
Noel Mier-Luna <nmierluna584@insite.4cd.edu>
2026-02-05 20:11:18 -0800

    7th Commit: Need to change how structs are added to vector.

--- 210-lab-4A.cpp
+++ 210-lab-4A.cpp
@@ -2,18 +2,19 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include <ctime> // For random number generation
+#include <ctime>   // For random number generation
+#include <iomanip> // For table formatting
 
 using namespace std;
 
 const int MIN_VALUE = 25;
 const int MAX_VALUE = 50;
 
-    struct Color
+struct Color
 {
-    int red = 0;
-    int green = 0; //All values initialized 
-    int blue = 0;
+    int red;
+    int green; // All values NOT initialized
+    int blue;
 };
 
 int main()
@@ -28,10 +29,16 @@ int main()
     for (int i = 0; i < n; i++)
     {
         Color tempColor = {rand() % 256, rand() % 256, rand() % 256}; // Random RGB values between 0 and 255
-        colorVector.push_back(tempColor); // Push struct object into vector
+        colorVector.push_back(tempColor);                             // Push struct object into vector
         cout << " Color " << i + 1 << ": (" << colorVector[i].red << ", " << colorVector[i].green << ", "
              << colorVector[i].blue << ")" << endl; // Output RGB values of current color
     }
-
+    cout << left << setw(10) << "Color #" << setw(10) << "Red" << setw(10) << "Green" << setw(10) << "Blue" << endl;
+    // Table headers
+    for (int i = 0; i < colorVector.size(); i++)
+    {
+        cout << left << setw(10) << i + 1 << setw(10) << colorVector[i].red << setw(10)
+             << colorVector[i].green << setw(10) << colorVector[i].blue << endl;
+    }
     return 0;
 }
